ec679724b9f5aa4fc90a99c0bfc272f60fd9247d
Siroshun09 <siro.shun8@gmail.com>
2026-08-02 13:52:36 +0900

    build: update to Java 25 and Gradle 9.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
 }
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 
 group = "net.okocraft"
 version = "1.0.0"
@@ -17,7 +17,7 @@ dependencies {
 }
 
 tasks.compileJava {
-    options.release.set(21)
+    options.release.set(25)
 }
 
 tasks.processResources {
